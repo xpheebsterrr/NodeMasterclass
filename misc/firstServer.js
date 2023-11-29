9b19c06d5ea97a4112d07afd29132246412e85c7
xpheebsterrr <89121683+xpheebsterrr@users.noreply.github.com>
2023-11-29 16:32:33 +0800

    slay

--- misc/firstServer.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req,res) => {
-    res.end('First Web Server Sheesh');
-});
-
-server.listen(3000, ()=> {
-    console.log('Server is started');
-});
--- misc/index.js
+++
@@ -1,7 +0,0 @@
-let language = 'JavaScript'
-
-console.log(language)
-
-for (let i=0; i<10; i++){
-    console.log(i);
-}
