50e2e316c18366e7a8c503f36532a1d2b4993c2f
xpheebsterrr <89121683+xpheebsterrr@users.noreply.github.com>
2023-12-07 16:50:37 +0800

    fix checkgroup according to specs

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -3,33 +3,46 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors")
 const db = require("../config/database")
 
 //Checkgroup function that returns a value to indicate if a user is in the group =>  /api/v1/checkGroup
-exports.checkGroup = catchAsyncErrors(async (req, res, next) => {
-   const { username, groupnames } = req.body
+async function checkGroup(userid, groupname) {
    //If a matching row is found, 1 is returned; if not, the query returns an empty result set.
-   const [result] = await db
-      .promise()
-      .query("SELECT 1 FROM accounts WHERE username = ? AND groupnames = ?", [
-         username,
-         groupnames
-      ])
-   //if user is in group isUserInGroup = true
-   const isUserInGroup = result.length > 0
-   //returns boolean value
-   res.json({
-      success: true,
-      message: `User ${username} is ${
-         isUserInGroup ? "" : "not "
-      }a ${groupnames}`,
-      isUserInGroup
-   })
-})
+   const [result] = await db.promise().query("SELECT groupnames FROM accounts WHERE username = ?", [userid])
+   // Check if the result is not empty and has groupnames
+   if (result.length > 0 && result[0].groupnames) {
+      result[0].groupnames = result[0].groupnames.split(",")
+      // Check if groupnames include the specified groupname
+      return result[0].groupnames.includes(groupname)
+   } else {
+      // Return false if result is empty or doesn't have groupnames
+      return false
+   }
+}
+
+// Exposes Checkgroup to route Checkgroup
+exports.checkingGroup = async (req, res) => {
+   //    const { username, groupnames } = req.body
+   const username = req.username
+   const group = req.body.groupnames
+   try {
+      const result = await checkGroup(username, group)
+      // Return successful check
+      return res.status(200).json({
+         result: result,
+         success: true,
+         message: `User ${username} is ${group}`
+      })
+   } catch (e) {
+      res.status(500).json({
+         success: false,
+         message: e
+      })
+      return
+   }
+}
 
 //Function that creates a group =>  /api/v1/createGroup
 exports.createGroup = catchAsyncErrors(async (req, res, next) => {
    const { groupnames } = req.body
-   await db
-      .promise()
-      .query("INSERT INTO `groups` (groupnames) VALUES (?)", [groupnames])
+   await db.promise().query("INSERT INTO `groups` (groupnames) VALUES (?)", [groupnames])
    res.json({
       success: true,
       message: "Group created successfully",
--- routes/router.js
+++ routes/router.js
@@ -9,33 +9,21 @@ const {
    updateUserPassword,
    updateUser
 } = require("../controllers/userController")
-const { createGroup, checkGroup } = require("../controllers/groupController")
-const {
-   protectedAdmin,
-   isAuthenticated,
-   isAuthorised
-} = require("../middlewares/protectedroutes")
+const { createGroup, checkingGroup } = require("../controllers/groupController")
+const { protectedAdmin, isAuthenticated, isAuthorised } = require("../middlewares/protectedroutes")
 const { loginUser, logoutUser } = require("../controllers/authController")
 
 //For Admins
 //getUsers
 router.route("/getUsers").post(isAuthenticated, isAuthorised("admin"), getUsers)
 //createUser
-router
-   .route("/createUser")
-   .post(isAuthenticated, isAuthorised("admin"), createUser)
+router.route("/createUser").post(isAuthenticated, isAuthorised("admin"), createUser)
 //toggleUserStatus (active/disabled)
-router
-   .route("/users/:username/toggle-status")
-   .put(isAuthenticated, isAuthorised("admin"), toggleIsActive)
+router.route("/users/:username/toggle-status").put(isAuthenticated, isAuthorised("admin"), toggleIsActive)
 //updateUser (all fields)
-router
-   .route("/users/:username")
-   .put(isAuthenticated, isAuthorised("admin"), protectedAdmin, updateUser)
+router.route("/users/:username").put(isAuthenticated, isAuthorised("admin"), protectedAdmin, updateUser)
 //createGroup
-router
-   .route("/createGroup")
-   .post(isAuthenticated, isAuthorised("admin"), createGroup)
+router.route("/createGroup").post(isAuthenticated, isAuthorised("admin"), createGroup)
 
 //For Users
 //login User
@@ -45,12 +33,10 @@ router.route("/logoutUser").post(isAuthenticated, logoutUser)
 //updateUserEmail
 router.route("/users/:username/email").put(isAuthenticated, updateUserEmail)
 //updateUserPassword
-router
-   .route("/users/:username/password")
-   .put(isAuthenticated, updateUserPassword)
+router.route("/users/:username/password").put(isAuthenticated, updateUserPassword)
 
 //Specs
 //Group functions
-router.route("/checkGroup").post(isAuthenticated, checkGroup)
+router.route("/checkGroup").post(isAuthenticated, checkingGroup)
 
 module.exports = router
