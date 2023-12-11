e371120f95407083fe5d56bf62d203bc4fa2ad53
xpheebsterrr <89121683+xpheebsterrr@users.noreply.github.com>
2023-12-12 02:50:18 +0800

    push

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,185 +8,189 @@ const saltRounds = 10 //number of salt rounds for bcrypt hashing
 
 // Get all Users  =>  /api/v1/getUsers (named as api for clarity)
 exports.getUsers = catchAsyncErrors(async (req, res, next) => {
-   const data = await db.promise().query("SELECT * FROM accounts")
-   res.json({
-      success: true,
-      message: "Retrieved ${data[0].length} users successfully",
-      data: data[0]
-   })
-   return
+    const data = await db.promise().query("SELECT * FROM accounts")
+    res.json({
+        success: true,
+        message: "Retrieved ${data[0].length} users successfully",
+        data: data[0]
+    })
+    return
 })
 
 function parseJwt(token) {
-   try {
-      // Split the token into its parts
-      const [header, payload, signature] = token.split(".")
+    try {
+        // Split the token into its parts
+        const [header, payload, signature] = token.split(".")
 
-      // Decode the payload
-      const base64 = payload.replace(/-/g, "+").replace(/_/g, "/")
-      const jsonPayload = decodeURIComponent(
-         atob(base64)
-            .split("")
-            .map(function (c) {
-               return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
-            })
-            .join("")
-      )
+        // Decode the payload
+        const base64 = payload.replace(/-/g, "+").replace(/_/g, "/")
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split("")
+                .map(function (c) {
+                    return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
+                })
+                .join("")
+        )
 
-      return JSON.parse(jsonPayload)
-   } catch (e) {
-      return null // Return null if token is invalid
-   }
+        return JSON.parse(jsonPayload)
+    } catch (e) {
+        return null // Return null if token is invalid
+    }
 }
 
 // Get one User  =>  /api/v1/getUser (named as api for clarity)
 exports.getUser = catchAsyncErrors(async (req, res, next) => {
-   const userData = parseJwt(req.body.access_token)
-   const data = await db.promise().query("SELECT * FROM accounts WHERE username = ?", userData.username)
-   res.json({
-      success: true,
-      message: "Retrieved user ${username} successfully",
-      data: data[0]
-   })
-   return
+    const userData = parseJwt(req.body.access_token)
+    const data = await db.promise().query("SELECT * FROM accounts WHERE username = ?", userData.username)
+    res.json({
+        success: true,
+        message: "Retrieved user ${username} successfully",
+        data: data[0]
+    })
+    return
 })
 
 // Create User  =>  /api/v1/CreateUser
 exports.createUser = catchAsyncErrors(async (req, res, next) => {
-   // const token = req.body.access_token // read the token from the cookie
-   const { username, password, email, groupnames } = req.body
-   //password complexity check
-   const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&()])[A-Za-z\d@$!%*?&()]{8,10}$/
-   if (!password.match(passwordRegex)) {
-      return res.status(400).json({
-         success: false,
-         message: "Password must be 8-10 characters long and include alphabets, numbers, and special characters."
-      })
-   }
-   // Check if the username already exists
-   const [existingUser] = await db.promise().query("SELECT * FROM accounts WHERE username = ?", [username])
-   if (existingUser.length > 0) {
-      return res.status(409).json({
-         // 409 Conflict
-         success: false,
-         message: `Error: User '${username}' already exists`
-      })
-   }
+    // const token = req.body.access_token // read the token from the cookie
+    const { username, password, email, groupnames } = req.body
+    //if no username
+    if (!username || username.trim() === "") {
+        return res.status(400).json({
+            success: false,
+            message: "Username is required."
+        })
+    }
+    //password complexity check
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&()])[A-Za-z\d@$!%*?&()]{8,10}$/
+    if (!password.match(passwordRegex)) {
+        return res.status(400).json({
+            success: false,
+            message: "Password must be 8-10 characters long and include alphabets, numbers, and special characters."
+        })
+    }
+    // Check if the username already exists
+    const [existingUser] = await db.promise().query("SELECT * FROM accounts WHERE username = ?", [username])
+    if (existingUser.length > 0) {
+        return res.status(409).json({
+            // 409 Conflict
+            success: false,
+            message: `Error: User '${username}' already exists`
+        })
+    }
 
-   //if no Group name
-   if (!groupnames || groupnames.trim() === "") {
-      return res.status(400).json({
-         success: false,
-         message: "Group name is required."
-      })
-   }
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
 
-   // Hash the password
-   const hashedPassword = await bcrypt.hash(password, saltRounds)
+    // Insert a new user into the accounts table
+    await db
+        .promise()
+        .query("INSERT INTO accounts (username, password, email, groupnames) VALUES (?, ?, ?, ?)", [
+            username,
+            hashedPassword,
+            email,
+            groupnames
+        ])
 
-   // Insert a new user into the accounts table
-   await db
-      .promise()
-      .query("INSERT INTO accounts (username, password, email, groupnames) VALUES (?, ?, ?, ?)", [
-         username,
-         hashedPassword,
-         email,
-         groupnames
-      ])
-
-   // Retrieve the newly created user
-   const [user] = await db.promise().query("SELECT * FROM accounts WHERE username = ?", [username])
-   res.json({
-      success: true,
-      message: "User is created successfully",
-      data: user[0]
-   })
-   return
+    // Retrieve the newly created user
+    const [user] = await db.promise().query("SELECT * FROM accounts WHERE username = ?", [username])
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user[0]
+    })
+    return
 })
 
 // Toggle User Status  =>  /api/v1/toggleIsActive
 exports.toggleIsActive = catchAsyncErrors(async (req, res, next) => {
-   const { username } = req.body
-   // Get the current 'isActive' status for the user
-   const [userData] = await db.promise().query("SELECT isActive FROM accounts WHERE username = ?", [username])
-   // Toggle the 'isActive' status
-   const newStatus = userData[0].isActive === "active" ? "disabled" : "active"
-   // Update the 'isActive' status for the user
-   const result = await db.promise().query("UPDATE accounts SET isActive = ? WHERE username = ?", [newStatus, username])
-   return res.json({
-      success: true,
-      message: "User status toggled successfully",
-      data: { username, isActive: newStatus }
-   })
+    const { username } = req.body
+    // Get the current 'isActive' status for the user
+    const [userData] = await db.promise().query("SELECT isActive FROM accounts WHERE username = ?", [username])
+    // Toggle the 'isActive' status
+    const newStatus = userData[0].isActive === "active" ? "disabled" : "active"
+    // Update the 'isActive' status for the user
+    const result = await db.promise().query("UPDATE accounts SET isActive = ? WHERE username = ?", [newStatus, username])
+    return res.json({
+        success: true,
+        message: "User status toggled successfully",
+        data: { username, isActive: newStatus }
+    })
 })
 
 // Update User  =>  /api/v1/updateUser
 exports.updateUser = catchAsyncErrors(async (req, res, next) => {
-   const { username, email, password, groupnames, isActive } = req.body
+    const { username, email, password, groupnames, isActive } = req.body
 
-   // Password complexity check and hashing
-   let hashedPassword
-   if (password) {
-      const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&()])[A-Za-z\d@$!%*?&()]{8,10}$/
-      if (!password.match(passwordRegex)) {
-         return res.status(400).json({
-            success: false,
-            message: "Password must be 8-10 characters long and include alphabets, numbers, and special characters."
-         })
-      }
-      hashedPassword = await bcrypt.hash(password, saltRounds) // using bcrypt to hash the password
-      await db
-         .promise()
-         .query("UPDATE accounts SET email = ?, password = ?, groupnames = ?, isActive = ? WHERE username = ?", [
-            email,
-            hashedPassword,
-            groupnames,
-            isActive,
-            username
-         ])
-   } else {
-      await db.promise().query("UPDATE accounts SET email = ?, groupnames = ?, isActive = ? WHERE username = ?", 
-      [email, groupnames, isActive, username])
-   }
+    // Password complexity check and hashing
+    let hashedPassword
+    if (password) {
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&()])[A-Za-z\d@$!%*?&()]{8,10}$/
+        if (!password.match(passwordRegex)) {
+            return res.status(400).json({
+                success: false,
+                message: "Password must be 8-10 characters long and include alphabets, numbers, and special characters."
+            })
+        }
+        hashedPassword = await bcrypt.hash(password, saltRounds) // using bcrypt to hash the password
+        await db
+            .promise()
+            .query("UPDATE accounts SET email = ?, password = ?, groupnames = ?, isActive = ? WHERE username = ?", [
+                email,
+                hashedPassword,
+                groupnames,
+                isActive,
+                username
+            ])
+    } else {
+        await db
+            .promise()
+            .query("UPDATE accounts SET email = ?, groupnames = ?, isActive = ? WHERE username = ?", [
+                email,
+                groupnames,
+                isActive,
+                username
+            ])
+    }
 
-   return res.json({
-      success: true,
-      message: "User updated successfully",
-      //avoid returning password info for secure coding
-      data: { username, email, groupnames, isActive }
-   })
+    return res.json({
+        success: true,
+        message: "User updated successfully",
+        //avoid returning password info for secure coding
+        data: { username, email, groupnames, isActive }
+    })
 })
 
 // Update user Email =>/api/v1/updateUserEmail
 exports.updateUserEmail = catchAsyncErrors(async (req, res, next) => {
-   const { username, email } = req.body
-   const data = await db.promise().query("UPDATE accounts SET email = ? WHERE username = ?", 
-   [email, username])
-   return res.json({
-      success: true,
-      message: "Email updated successfully",
-      data: { username, email }
-   })
+    const { username, email } = req.body
+    const data = await db.promise().query("UPDATE accounts SET email = ? WHERE username = ?", [email, username])
+    return res.json({
+        success: true,
+        message: "Email updated successfully",
+        data: { username, email }
+    })
 })
 
 // Update user Password =>/api/v1/updateUserPassword
 exports.updateUserPassword = catchAsyncErrors(async (req, res, next) => {
-   const { username, password } = req.body
-   let hashedPassword
-   if (password) {
-      const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&()])[A-Za-z\d@$!%*?&()]{8,10}$/
-      if (!password.match(passwordRegex)) {
-         return res.status(400).json({
-            success: false,
-            message: "Password must be 8-10 characters long and include alphabets, numbers, and special characters."
-         })
-      }
-      hashedPassword = await bcrypt.hash(password, saltRounds)
-   }
-   await db.promise().query("UPDATE accounts SET password = ? WHERE username = ?", [hashedPassword, username])
-   return res.json({
-      success: true,
-      message: "Password updated successfully",
-      data: { username, password }
-   })
+    const { username, password } = req.body
+    let hashedPassword
+    if (password) {
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&()])[A-Za-z\d@$!%*?&()]{8,10}$/
+        if (!password.match(passwordRegex)) {
+            return res.status(400).json({
+                success: false,
+                message: "Password must be 8-10 characters long and include alphabets, numbers, and special characters."
+            })
+        }
+        hashedPassword = await bcrypt.hash(password, saltRounds)
+    }
+    await db.promise().query("UPDATE accounts SET password = ? WHERE username = ?", [hashedPassword, username])
+    return res.json({
+        success: true,
+        message: "Password updated successfully",
+        data: { username, password }
+    })
 })
--- middlewares/protectedroutes.js
+++ middlewares/protectedroutes.js
@@ -4,69 +4,87 @@ const db = require("../config/database")
 /** Returns true if JWT is valid */
 //ensures that user is logged in
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-   //check for cookie called access_token to grant access to controller
-   const token = req.body.access_token // read the token from the cookie
-   console.log(token)
-   if (token == null) {
-      return res.sendStatus(401) // if there isn't any token
-   }
-   try {
-      const data = jwt.verify(token, process.env.JWT_SECRET)
-      req.username = data.username
-      const result = await db.promise().query("SELECT * FROM accounts WHERE username = ?", [req.username])
-      req.userDetails = result[0][0]
-      console.log("req.userDetails", req.userDetails)
-      // req.groupnames = data.groupnames
-      return next() // pass the execution off to whatever request the client intended
-   } catch (e) {
-      console.log("authentication error", e)
-      return res.sendStatus(403) // if the token has expired or is invalid
-   }
+    //check for cookie called access_token to grant access to controller
+    const token = req.body.access_token // read the token from the cookie
+    console.log("isAuthenticated", token)
+    if (token == null) {
+        return res.sendStatus(401) // if there isn't any token
+    }
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+        req.username = data.username
+        const result = await db.promise().query("SELECT * FROM accounts WHERE username = ?", [req.username])
+        req.userDetails = result[0][0]
+        //   if (req.userDetails.isActive === "disabled") {
+        //       return res.sendStatus(401) //user is disabled
+        //   }
+        // req.groupnames = data.groupnames
+        return next() // pass the execution off to whatever request the client intended
+    } catch (e) {
+        console.log("authentication error", e)
+        return res.sendStatus(403) // if the token has expired or is invalid
+    }
 })
 
 //ensures user had the valid permissions of the user group
 exports.isAuthorised = (...groups) => {
-   return (req, res, next) => {
-      const { groupnames } = req.userDetails
-      const requiredGroup = new Set(groups)
-      let isAuthorised = false
-      //User can be in multiple groups eg. user,admin,dev (string) hence we need to split them into an array
-      if (groupnames) {
-         const userGroups = groupnames.split(",")
-         //if any of user's group is in the req role, user authorised
-         isAuthorised = userGroups.some(group => requiredGroup.has(group))
-         if (isAuthorised) {
-            //user is authorized, continue to next middleware
-            next()
-            return
-         } else {
+    return (req, res, next) => {
+        const { groupnames } = req.userDetails
+        const requiredGroup = new Set(groups)
+        let isAuthorised = false
+        //User can be in multiple groups eg. user,admin,dev (string) hence we need to split them into an array
+        if (groupnames) {
+            const userGroups = groupnames.split(",")
+            //if any of user's group is in the req role, user authorised
+            isAuthorised = userGroups.some(group => requiredGroup.has(group))
+            if (isAuthorised) {
+                console.log("isAuthorised")
+                //user is authorized, continue to next middleware
+                next()
+                return
+            } else {
+                res.json({
+                    success: false,
+                    message: "Error: User is not authorised."
+                })
+                return
+            }
+        } else {
+            //if groupnames not provided in req
             res.json({
-               success: false,
-               message: "Error: User is not authorised."
+                success: false,
+                message: "Error: missing groupname in req body."
             })
             return
-         }
-      } else {
-         //if groupnames not provided in req
-         res.json({
-            success: false,
-            message: "Error: missing groupname in req body."
-         })
-         return
-      }
-   }
+        }
+    }
 }
 
 //ensures that admins cannot edit admins
 exports.protectedAdmin = async (req, res, next) => {
-   const { groupnames } = req.userDetails
-   if (groupnames === "admin" && req.user.username != "admin") {
-      res.json({
-         success: false,
-         message: "Error: Admin is not editable."
-      })
-      return
-   } else {
-      next()
-   }
+    const { groupnames } = req.userDetails
+    console.log("123", req.userDetails)
+    if (groupnames === "admin" && req.user.username != "admin") {
+        res.json({
+            success: false,
+            message: "Error: Admin is not editable."
+        })
+        return
+    } else {
+        next()
+    }
 }
+
+//ensures that disabled users get kicked out
+// exports.isActive = async (req, res, next) => {
+//    const { groupnames } = req.userDetails
+//    if (groupnames === "admin" && req.user.username != "admin") {
+//        res.json({
+//            success: false,
+//            message: "Error: Admin is not editable."
+//        })
+//        return
+//    } else {
+//        next()
+//    }
+// }
